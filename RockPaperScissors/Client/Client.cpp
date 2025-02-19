861630046d32e756cc1e4883e9420fb35cff4590
Zeskky <05alexgt@gmail.com>
2025-02-19 12:32:34 +0100

    C'est fini!

--- RockPaperScissors/Client/Client.cpp
+++ RockPaperScissors/Client/Client.cpp
@@ -4,7 +4,13 @@
 #include <string>
 #pragma comment(lib, "ws2_32.lib")
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 2)
+    {
+        std::cout << "Error: no se ha proporcionado la dirección IP del servidor. Saliendo del programa." << std::endl;
+        return 1;
+    }
+
     WSADATA wsa;
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
     {
@@ -17,33 +23,36 @@ int main() {
     sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    std::cout << "Esperando oponente..." << std::endl;
 
     std::string join_msg = "join";
-    sendto(client_socket, join_msg.c_str(), join_msg.size(), 0, (sockaddr*)&server_addr, sizeof(server_addr));
+    sendto(client_socket, join_msg.c_str(), join_msg.size(), 0, (SOCKADDR*)&server_addr, sizeof(server_addr));
 
-    char buffer[512];
+    char buffer1[512];
     int server_len = sizeof(server_addr);
     int bytes = 0;
-    bytes = recvfrom(client_socket, buffer, 512, 0, (sockaddr*)&server_addr, &server_len);
+    bytes = recvfrom(client_socket, buffer1, 512, 0, (SOCKADDR*)&server_addr, &server_len);
     if (bytes > 0)
     {
-        buffer[bytes] = '\0';
-        std::cout << buffer << std::endl;
+        buffer1[bytes] = '\0';
+        std::cout << buffer1;
     }
 
     std::string move;
     // std::cout << "Tu jugada (piedra, papel, tijeras): ";
     std::cin >> move;
-    sendto(client_socket, move.c_str(), move.size(), 0, (sockaddr*)&server_addr, sizeof(server_addr));
+    sendto(client_socket, move.c_str(), move.size(), 0, (SOCKADDR*)&server_addr, sizeof(server_addr));
 
-    bytes = recvfrom(client_socket, buffer, 512, 0, (sockaddr*)&server_addr, &server_len);
+    char buffer2[512];
+    bytes = recvfrom(client_socket, buffer2, 512, 0, (SOCKADDR*)&server_addr, &server_len);
     if (bytes > 0)
     {
-        buffer[bytes] = '\0';
-        std::cout << buffer << std::endl;
+        buffer2[bytes] = '\0';
+        // std::cout << buffer2 << std::endl;
     }
-    std::cout << "Resultado: " << buffer << std::endl;
+    std::cout << "Resultado: " << buffer2 << std::endl;
 
     closesocket(client_socket);
     WSACleanup();
--- RockPaperScissors/Server/Server.cpp
+++ RockPaperScissors/Server/Server.cpp
@@ -14,8 +14,8 @@ std::vector<SOCKADDR_IN> waiting_clients;
 
 void handle_client(SOCKET server_socket, SOCKADDR_IN client1, SOCKADDR_IN client2) {
 
+    char buffer[512];
     mtx.lock();
-    char buffer1[512], buffer2[512];
     int client1_len = sizeof(client1);
     int client2_len = sizeof(client2);
 
@@ -24,12 +24,14 @@ void handle_client(SOCKET server_socket, SOCKADDR_IN client1, SOCKADDR_IN client
     sendto(server_socket, msg.c_str(), msg.size(), 0, (sockaddr*)&client2, client2_len);
 
     // Solicitar jugada a cliente 1
-    int bytes1 = recvfrom(server_socket, buffer1, 512, 0, (sockaddr*)&client1, &client1_len);
-    std::string move1 = buffer1;
+    int bytes1 = recvfrom(server_socket, buffer, 512, 0, (sockaddr*)&client1, &client1_len);
+    std::string move1 = buffer;
+    buffer[bytes1] = '\0';
 
     // Solicitar jugada a cliente 2
-    int bytes2 = recvfrom(server_socket, buffer2, 512, 0, (sockaddr*)&client2, &client2_len);
-    std::string move2 = buffer2;
+    int bytes2 = recvfrom(server_socket, buffer, 512, 0, (sockaddr*)&client2, &client2_len);
+    std::string move2 = buffer;
+    buffer[bytes2] = '\0';
 
     // Determinar ganador
     std::string result;
@@ -37,10 +39,12 @@ void handle_client(SOCKET server_socket, SOCKADDR_IN client1, SOCKADDR_IN client
     else if ((move1 == "piedra" && move2 == "tijeras") ||
         (move1 == "papel" && move2 == "piedra") ||
         (move1 == "tijeras" && move2 == "papel")) {
-        result = "Gana Cliente 1!";
+        result = "Gana Cliente ";
+        result += inet_ntoa(client1.sin_addr);
     }
     else {
-        result = "Gana Cliente 2!";
+        result = "Gana Cliente ";
+        result += inet_ntoa(client2.sin_addr);
     }
 
     // Enviar resultados
@@ -73,17 +77,19 @@ void server() {
     std::cout << "Servidor esperando conexiones en el puerto 8080..." << std::endl;
 
     while (true) {
+        char buffer[512];
         SOCKADDR_IN client_addr;
         int addr_len = sizeof(client_addr);
-        char buffer[512];
         int bytes = recvfrom(server_socket, buffer, 512, 0, (sockaddr*)&client_addr, &addr_len);
         buffer[bytes] = '\0';
         std::string msg = buffer;
 
         if (msg == "join") {
+            // Agregamos al jugador a la lista de espera
             waiting_clients.push_back(client_addr);
-            std::cout << "Cliente conectado. ID de cliente: " << waiting_clients.size() << std::endl;
+            std::cout << "Cliente "  << inet_ntoa(client_addr.sin_addr) << " conectado. ID de cliente: " << waiting_clients.size() << std::endl;
             if (waiting_clients.size() == 2) {
+                // Emparejamos a ambos clientes
                 SOCKADDR_IN client1 = waiting_clients[0];
                 SOCKADDR_IN client2 = waiting_clients[1];
                 waiting_clients.clear();
